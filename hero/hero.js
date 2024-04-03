d835faeea897729a5de6004bf85390f48c4c5ff3
Ulya <auluk2005@gmail.com>
2024-04-03 19:31:48 +0600

    lololowka

---
+++ hero/hero.js
@@ -0,0 +1,14 @@
+var mobileMenu = document.querySelector(".mobileMenu");
+var mobileMenuOpened = document.querySelector(".mobileMenuOpened");
+var mobileMenuClose = document.querySelector(".mobileMenuClose")
+
+
+mobileMenu.addEventListener("click", function (){
+        mobileMenuOpened.classList.add("visible");
+        document.body.classList.add("overflowHidden")
+});
+
+mobileMenuClose.addEventListener("click", function(){
+    mobileMenuOpened.classList.remove("visible");
+    document.body.classList.remove("overflowHidden")
+})
